cf4f6c2bad71d0c10cb99c0692edfd3ac8134be5
shaangontia <shaan.ce@gmail.com>
2022-12-14 21:25:04 +0100

    added installation script

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+cd container
+echo ">>>>Installing dependencies for Container app"
+npm i .
+cd ../app-1
+echo ">>>>Installing dependencies for app-1"
+npm i .
+cd ../app-2
+echo ">>>>Installing dependencies for app-2"
+npm i .
